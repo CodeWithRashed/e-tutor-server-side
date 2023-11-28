e24e86f8f8f2c3d9d35632f886f7ef6fcc4164e7
Md Rashed Ali <tools.rashed@gmail.com>
2023-11-28 20:56:27 +0600

    getting teacher request

--- src/api/v1/users/controllers/getAllUsers.js
+++ src/api/v1/users/controllers/getAllUsers.js
@@ -1,16 +1,22 @@
 const findAllUser = require("../../../../lib/user/findAllUser");
 
-const getAllUser= async (req, res) => {
-    let query = {}
-    const isTeacherRequest = req.query.isTeacherRequest
-    const page = parseInt(req.query.page) || 1;
-    const pageSize = parseInt(req.query.pageSize);
-    if (isTeacherRequest === 'true') {
-        query = { isTeacherRequest: true };
-      }
-    const dbAllUserData= await findAllUser(query, page, pageSize)
-    res.send(dbAllUserData);
-}
+const getAllUser = async (req, res) => {
+  let query = {};
+  const isTeacherRequest = req.query.isTeacherRequest;
+  const page = parseInt(req.query.page) || 1;
+  const pageSize = parseInt(req.query.pageSize);
 
+  if (isTeacherRequest == "All") {
+    query = {
+      isTeacherRequest: { $in: ["Pending", "Accepted", "Rejected"] },
+    };
+  }
+  if (isTeacherRequest == "Pending") {
+    query = { isTeacherRequest: "Pending" };
+  }
 
-module.exports = getAllUser
+  const dbAllUserData = await findAllUser(query, page, pageSize);
+  res.send(dbAllUserData);
+};
+
+module.exports = getAllUser;
