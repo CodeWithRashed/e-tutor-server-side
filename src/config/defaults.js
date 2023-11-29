cb9f3f3a523854e0dfb5b3b445f6bd682512b320
Md Rashed Ali <tools.rashed@gmail.com>
2023-11-29 10:09:20 +0600

    cores client update

--- src/config/defaults.js
+++ src/config/defaults.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const config = {
   LOCAL_CLIENT: process.env.LOCAL_CLIENT,
   LOCAL_CLIENT_SECOND: process.env.LOCAL_CLIENT_SECOND,
-  CLIENT: process.env.CLIENT,
+  CLIENT_FIREBASE: process.env.CLIENT_FIREBASE,
 };
 
 module.exports = Object.freeze(config);
--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -9,7 +9,7 @@ const applyMiddleware = (app)=>{
 app.use(cors({
     origin: [
         LOCAL_CLIENT,
-        CLIENT,
+        CLIENT_FIREBASE,
         LOCAL_CLIENT_SECOND
     ],
     credentials: true
